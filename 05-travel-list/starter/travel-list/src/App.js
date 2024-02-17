5b964e4e5a76d19dad06b86eada226421e2bb6ec
ZeynalliZeynal <zzeynalli446@gmail.com>
2024-02-17 23:11:06 +0400

    stats and actions on the list

--- 05-travel-list/starter/travel-list/src/App.js
+++ 05-travel-list/starter/travel-list/src/App.js
@@ -1,15 +1,34 @@
-const initialItems = [
-  { id: 1, description: 'Passports', quantity: 2, packed: false },
-  { id: 2, description: 'Socks', quantity: 12, packed: false },
-]
+import { useState } from 'react'
 
 export default function App() {
+  const [items, setItems] = useState([])
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item])
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id))
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item,
+      ),
+    )
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
-      <Stats />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+      />
+      <Stats items={items} />
     </div>
   )
 }
@@ -17,35 +36,96 @@ export default function App() {
 function Logo() {
   return <h1>🌴 Far Away 💼</h1>
 }
-function Form() {
+function Form({ onAddItems }) {
+  const [description, setDescription] = useState('')
+  const [quantity, setQuantity] = useState(1)
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    if (!description) return
+    const newItem = { description, quantity, packed: false, id: new Date() }
+    onAddItems(newItem)
+    setDescription('')
+    setQuantity(1)
+  }
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
+
+      <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button type="submit">Add</button>
     </form>
   )
 }
 
-function Item({ item }) {
-  return <li>{item.description}</li>
-}
-
-function PackingList() {
+function PackingList({ items, onDeleteItem, onToggleItem }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
-          <Item item={item} />
+        {items.map((item) => (
+          <Item
+            item={item}
+            key={item.id}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+          />
         ))}
       </ul>
     </div>
   )
 }
-function Stats() {
+
+function Item({ item, onDeleteItem, onToggleItem }) {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
+    </li>
+  )
+}
+
+function Stats({ items }) {
+  const numItems = items.length
+  const numPacked = items.filter((item) => item.packed).length
+  const itemPerc = (numPacked / numItems) * 100
+
+  if (!numItems)
+    return (
+      <footer className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </footer>
+    )
+
   return (
     <footer className="stats">
       <em>
-        You got everything! Ready to go ✈️ 💼 You have items on your list, and
-        you already packed
+        {itemPerc < 100
+          ? `📦 You have ${numItems} items on your list, and
+        ${
+          numPacked
+            ? ` you already packed ${numPacked} (${itemPerc}%)`
+            : ' nothing is packed yet.'
+        }`
+          : 'You got everything! Ready to go! 🚢'}
       </em>
     </footer>
   )
