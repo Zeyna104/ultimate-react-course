43f1b7ea9987b841eab273899dcbf4f072f25ae7
Zeynalli Zeynal <zzeynalli446@gmail.com>
2024-08-05 03:15:05 +0400

    not found and error handling

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/_components/Navigation.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/_components/Navigation.js
@@ -2,28 +2,31 @@ import Link from "next/link";
 
 export default function Navigation() {
   return (
-    <nav className="z-10 text-xl">
-      <ul className="flex gap-16 items-center">
+    <nav className='z-10 text-xl'>
+      <ul className='flex items-center gap-16'>
         <li>
           <Link
-            href="/cabins"
-            className="hover:text-accent-400 transition-colors"
+            href='/cabins'
+            className='transition-colors hover:text-accent-400'
           >
             Cabins
           </Link>
         </li>
         <li>
-          <a href="/about" className="hover:text-accent-400 transition-colors">
+          <Link
+            href='/about'
+            className='transition-colors hover:text-accent-400'
+          >
             About
-          </a>
+          </Link>
         </li>
         <li>
-          <a
-            href="/account"
-            className="hover:text-accent-400 transition-colors"
+          <Link
+            href='/account'
+            className='transition-colors hover:text-accent-400'
           >
             Guest area
-          </a>
+          </Link>
         </li>
       </ul>
     </nav>
