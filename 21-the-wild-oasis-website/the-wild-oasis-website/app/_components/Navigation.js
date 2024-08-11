4bb319e338a113f99ccb05152510d523b21a906b
Zeynalli Zeynal <zzeynalli446@gmail.com>
2024-08-11 16:07:32 +0400

    auth routes

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/_components/Navigation.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/_components/Navigation.js
@@ -1,6 +1,9 @@
 import Link from "next/link";
+import { auth } from "../_lib/auth";
 
-export default function Navigation() {
+export default async function Navigation() {
+  const session = await auth();
+
   return (
     <nav className='z-10 text-xl'>
       <ul className='flex items-center gap-16'>
@@ -21,12 +24,26 @@ export default function Navigation() {
           </Link>
         </li>
         <li>
-          <Link
-            href='/account'
-            className='transition-colors hover:text-accent-400'
-          >
-            Guest area
-          </Link>
+          {session?.user?.image ? (
+            <Link
+              href='/account'
+              className='flex items-center gap-4 transition-colors hover:text-accent-400'
+            >
+              <img
+                src={session.user.image}
+                alt={session.user.name}
+                className='h-8 rounded-full'
+              />
+              Guest area
+            </Link>
+          ) : (
+            <Link
+              href='/account'
+              className='transition-colors hover:text-accent-400'
+            >
+              Guest area
+            </Link>
+          )}
         </li>
       </ul>
     </nav>
--- 21-the-wild-oasis-website/the-wild-oasis-website/app/_components/Reservation.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/_components/Reservation.js
@@ -5,6 +5,8 @@ import {
   getCabin,
   getSettings,
 } from "@/app/_lib/data-service";
+import { auth } from "../_lib/auth";
+import LoginMessage from "./LoginMessage";
 
 export default async function Reservation({ cabin }) {
   const [settings, bookedDates] = await Promise.all([
@@ -12,15 +14,20 @@ export default async function Reservation({ cabin }) {
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
   ]);
+  const session = await auth();
 
   return (
-    <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
+    <div className='grid grid-cols-2 border border-primary-800 min-h-[400px]'>
       <DateSelector
         settings={settings}
         bookedDates={bookedDates}
         cabin={cabin}
       />
-      <ReservationForm cabin={cabin} />
+      {session?.user ? (
+        <ReservationForm user={session.user} cabin={cabin} />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   );
 }
